010e8ec5ee157fec4a6aec88b2d502b4a712fe7f
fabriciobrag <fabriciobedeschi@gmail.com>
2013-04-24 10:28:36 -0300

    initial app

---
+++ app/src/tcc/tcc1/AjudaActivity.java
@@ -0,0 +1,47 @@
+package tcc.tcc1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Tela de ajuda do aplicativo
+ * @authors Fabrício e Manoel
+ *
+ */
+public class AjudaActivity extends Activity {
+
+	private Button okButton;
+	private TextView textoAjudaTextView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.ajuda_activity);
+		carregaVariaveis();
+		listeners();
+	}
+	
+	/**
+	 * Inicaliza as variaveis
+	 */
+	private void carregaVariaveis (){
+		okButton = (Button)findViewById(R.id.ok_button);
+		textoAjudaTextView = (TextView)findViewById(R.id.ajuda_textView);
+		textoAjudaTextView.setText(R.string.texto_ajuda);
+	}
+	
+	/**
+	 * Metodos listener dos objetos
+	 */
+	private void listeners(){
+		okButton.setOnClickListener(new View.OnClickListener() {	
+			public void onClick(View v) {
+				finish(); //retorna para a activity mais acima na pilha
+			}
+		});
+	}
+}
---
+++ app/src/tcc/tcc1/BoasVindasActivity.java
@@ -0,0 +1,66 @@
+package tcc.tcc1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+/**
+ * Tela incial do aplicativo.
+ * @authors Fabrício e Manoel
+ *
+ */
+public class BoasVindasActivity extends Activity{
+	
+	private ImageButton startImageButton, helpImagepButton;
+
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.boas_vindas_activity);
+        carregaVariaveis();
+        listeners();
+	}
+	
+	/**
+	 * Realiza a inicialização das variáveis
+	 */
+	private void carregaVariaveis (){
+		helpImagepButton = (ImageButton)findViewById(R.id.help_imageButtonBoasVindas);
+		startImageButton = (ImageButton)findViewById(R.id.start_ImageButton);
+	}
+	
+	/**
+	 * Conjunto de metodos listeners
+	 */
+	private void listeners (){
+		startImageButton.setOnClickListener(new View.OnClickListener() {	
+			public void onClick(View v) {
+				mainActivity();
+			}
+		});
+		
+		helpImagepButton.setOnClickListener(new View.OnClickListener() {	
+			public void onClick(View v) {
+				helpActivity();
+			}
+		});
+	}
+	
+	/**
+	 * Inicia a activity principal
+	 */
+	private void mainActivity(){
+		Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+	}
+	
+	/**
+	 * Inicia a activity de ajuda
+	 */
+	private void helpActivity(){
+		Intent i = new Intent(this, AjudaActivity.class);
+        startActivity(i);
+	}
+}
