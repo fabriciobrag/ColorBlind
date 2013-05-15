9901c7e8968244ebada5257b6503392ad39b017c
Fabricio Bragunci Bedeschi <fabriciobedeschi@gmail.com>
2013-05-15 17:04:11 -0300

    Check network activity

--- app/src/tcc/tcc1/HelloActivity.java
+++ app/src/tcc/tcc1/HelloActivity.java
@@ -1,8 +1,24 @@
 package tcc.tcc1;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -15,6 +31,9 @@ import android.widget.ImageButton;
 public class HelloActivity extends Activity {
 
 	private ImageButton startImageButton, helpImagepButton;
+	private DefaultHttpClient httpclient;
+	private HttpPost httppost;
+	private ArrayList<NameValuePair> nameValuePairs;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,6 +41,54 @@ public class HelloActivity extends Activity {
 		setContentView(R.layout.hello_activity);
 		carregaVariaveis();
 		listeners();
+		
+		Log.i(ManagerTest.APP_NAME, ""+ hasActiveInternetConnection());
+		
+		if (hasActiveInternetConnection()) {
+			
+			httpclient = new DefaultHttpClient();
+            httppost = new HttpPost("http://10.0.2.2/project/insert.php");
+
+            try
+            {
+                nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("age", "8"));
+                nameValuePairs.add(new BasicNameValuePair("result", "10"));
+
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse response = httpclient.execute(httppost);
+                response.getHeaders("teste");
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+		}
+	}
+	
+	private boolean hasActiveInternetConnection (){
+	    if (isNetworkAvailable()) {
+	        try {
+	            HttpURLConnection urlc = (HttpURLConnection) (new URL("http://www.google.com").openConnection());
+	            urlc.setRequestProperty("User-Agent", "Test");
+	            urlc.setRequestProperty("Connection", "close");
+	            urlc.setConnectTimeout(1500); 
+	            urlc.connect();
+	            return (urlc.getResponseCode() == 200);
+	        } catch (IOException e) {
+	            Log.e(ManagerTest.APP_NAME, "Error checking internet connection", e);
+	        }
+	    } else {
+	        Log.d(ManagerTest.APP_NAME, "No network available!");
+	    }
+	    return false;
+	}
+	
+	private boolean isNetworkAvailable() {
+	    ConnectivityManager connectivityManager 
+	          = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+	    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+	    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
 	}
 
 	private void carregaVariaveis() {
